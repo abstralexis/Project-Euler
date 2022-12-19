9a748d091c412f703a57ca105436798d0a009388
AlexisComix <Thecomixguy@hotmail.com>
2022-12-19 13:31:02 +0000

    Add Challenge 1 examples in multiple languages

---
+++ 1_Multiples_of_3_or_5/c/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    int sum = 0;
+    for (int i = 0; i < 1000; i++) {
+        if (i % 3 == 0 || i % 5 == 0) {
+            sum += i;
+        }
+    }
+    printf("%i", sum);
+}
---
+++ 1_Multiples_of_3_or_5/java/Main.java
@@ -0,0 +1,11 @@
+class Main {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 0; i < 1000; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                sum += i;
+            }
+        }
+        System.out.println("sum: " + sum);
+    }
+}
