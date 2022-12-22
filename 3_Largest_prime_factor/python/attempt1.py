bcdc47269ede900533fbf63d89091d7c0e167d33
AlexisComix <thecomixguy@hotmail.com>
2022-12-22 16:32:12 +0000

    Slow bad attempt

---
+++ 3_Largest_prime_factor/python/attempt1.py
@@ -0,0 +1,32 @@
+"""
+This implementation is really slow :c there are better ways
+of doing it but it does not looks as nicely functional and
+I am too stubborn to copy one and then rewrite it in my own
+way :(
+"""
+
+from math import sqrt
+
+def is_prime(n: int) -> bool:
+    if n % 2 == 0:
+        return True
+    
+    for i in range(3, int(sqrt(n) + 1), 2):
+        if n % i == 0:
+            return False
+    return True
+
+def is_factor_of(n: int):
+    def is_factor(i) -> bool:
+        return n % i == 0
+    return is_factor
+
+def highest_prime_factor(n: int):
+    return max(filter(is_prime, filter(is_factor_of(n), range(2, n//2))))
+
+def main():
+    print(f"{highest_prime_factor(13195)}")
+
+if __name__ == "__main__":
+    main()
+    
